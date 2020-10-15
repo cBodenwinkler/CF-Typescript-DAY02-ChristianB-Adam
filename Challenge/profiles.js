434d4e40b96c92eab4133cd13437c1d59f79ced6
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-10-15 13:42:58 +0200

    Challenge w jQuery

--- Challenge/profiles.js
+++ Challenge/profiles.js
@@ -1,33 +1,54 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+var profileArr = [];
 var Profiles = /** @class */ (function () {
-    function Profiles(quote) {
+    function Profiles(quote, img, name, age, location, hobbies, favMusic) {
         this.quote = "";
         this.quote = quote;
+        this.img = img;
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.hobbies = hobbies;
+        this.favMusic = favMusic;
+        profileArr.push(this);
     }
+    Profiles.prototype.render = function (i) {
+        return "\n            <div class=\"card col-3\" id=\"favCard" + i + "\" style=\"width: 18rem;\">\n                <img src=\"" + this.img + "\" class=\"card-img-top\" alt=\"...\">\n                <div class=\"card-body myClass" + i + "\">\n                    <p class=\"card-text removeP" + i + "\">" + this.quote + "</p>\n                </div>\n            </div>";
+    };
+    Profiles.prototype.render2 = function () {
+        return "\n        <p>Name: " + this.name + "</p>\n        <p>Age: " + this.age + "</p>\n        <p>Location: " + this.location + "</p>\n        <p>Hobbies: " + this.hobbies + "</p>\n        <p>Favourite Music: " + this.favMusic + "</p>\n        ";
+    };
     return Profiles;
 }());
-var Favourite = /** @class */ (function (_super) {
-    __extends(Favourite, _super);
-    function Favourite(quote, name, age, location, hobbies, favMusic) {
-        var _this = _super.call(this, quote) || this;
-        _this.name = name;
-        _this.age = age;
-        _this.location = location;
-        _this.hobbies = hobbies;
-        _this.favMusic = favMusic;
-        return _this;
-    }
-    return Favourite;
-}(Profiles));
+// Profile Creation: quote, img, name, age, location, hobbies, favMusic
+new Profiles("See you next Beer", 'https://w7.pngwing.com/pngs/384/123/png-transparent-pewdiepie-funny-face-youtuber-youtube-face-head-hair.png', "Hans", 30, 'Sweden', 'PC Games', 'Rock');
+// "Hans", 30, 'Sweden', 'PC Games', 'Rock'
+new Profiles("Have yourself an erratic day", 'https://lefunny.net/wp-content/uploads/2014/08/funny-people-pics.jpg', "Horst", 35, 'AZ', 'Hair Styling', 'Hip Hop');
+new Profiles("Be extravagant", 'https://image.shutterstock.com/image-photo/funny-portrait-hero-260nw-410898763.jpg', "Heribert", 45, 'Austria', 'Photography', 'All');
+new Profiles("Dads will soon spread immortality", 'https://i.ytimg.com/vi/7__ZbrF58kI/hqdefault.jpg', "Olaf", 28, 'Japan', 'Photography', 'Metal');
+new Profiles("Always investigate a kick in the groin", 'https://4.bp.blogspot.com/-8va7RpZNSF8/UpIZdO988PI/AAAAAAAAFG4/s63zq2d9eQw/s1600/new+funny+girls.jpg', "Eins", 23, 'Austria', 'Travel', 'Pop');
+new Profiles("The answer to world hunger is nihilism", 'https://www.askideas.com/media/17/Carrie-Underwood-With-Upside-Down-Head-Funny-Photoshopped-Image.png', "Zwei", 32, 'NV', 'Electronics', 'Country');
+new Profiles("Respect a fraud", 'https://images.unsplash.com/photo-1583802625550-f9f11adcbe35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', "Drei", 21, 'South Africa', 'Travel', 'Rock');
+new Profiles("Fears are nice", 'https://images.unsplash.com/photo-1601705776515-b55faf59eb09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', "Vier", 92, 'Austria', 'Photography', 'All');
+console.table(profileArr);
+for (var i in profileArr) {
+    $('#result').append(profileArr[i].render(i));
+}
+var _loop_1 = function (i) {
+    $("#favCard" + i).on("click", function () {
+        $(".removeP" + i).remove(".removeP" + i);
+        $(".myClass" + i).html(profileArr[i].render2());
+    });
+};
+for (var i in profileArr) {
+    _loop_1(i);
+}
+var _loop_2 = function (i) {
+    $("#favCard" + i).on("dblclick", function () {
+        $(".favourite").html(profileArr[i].render(i));
+        $(".removeP" + i).remove(".removeP" + i);
+        $(".myClass" + i).html(profileArr[i].render2(i));
+    });
+};
+for (var i in profileArr) {
+    _loop_2(i);
+}
--- Challenge/profiles.ts
+++ Challenge/profiles.ts
@@ -1,22 +1,89 @@
-class Profiles {
+const profileArr:Array<any>=[];
+class Profiles{
     quote = "";
-    constructor(quote) {
-        this.quote = quote
-    }
-}
-
-class Favourite extends Profiles {
+    img?:string;
     name: "";
     age: "";
     location: "";
     hobbies: "";
     favMusic: "";
-    constructor(quote,name,age,location,hobbies,favMusic) {
-        super(quote);
+    constructor(quote,img,name,age,location,hobbies,favMusic) {
+        this.quote = quote;
+        this.img = img;
         this.name = name;
         this.age = age;
         this.location = location;
         this.hobbies = hobbies;
         this.favMusic = favMusic;
+        profileArr.push(this);
     }
+    render(i) {
+        return `
+            <div class="card col-3" id="favCard${i}" style="width: 18rem;">
+                <img src="${this.img}" class="card-img-top" alt="...">
+                <div class="card-body myClass${i}">
+                    <p class="card-text removeP${i}">${this.quote}</p>
+                </div>
+            </div>`
+    }
+
+    render2() {
+        return `
+        <p>Name: ${this.name}</p>
+        <p>Age: ${this.age}</p>
+        <p>Location: ${this.location}</p>
+        <p>Hobbies: ${this.hobbies}</p>
+        <p>Favourite Music: ${this.favMusic}</p>
+        `
+    }
+
 }
+
+
+// Profile Creation: quote, img, name, age, location, hobbies, favMusic
+new Profiles ("See you next Beer", 'https://w7.pngwing.com/pngs/384/123/png-transparent-pewdiepie-funny-face-youtuber-youtube-face-head-hair.png', "Hans", 30, 'Sweden', 'PC Games', 'Rock');
+
+// "Hans", 30, 'Sweden', 'PC Games', 'Rock'
+
+new Profiles("Have yourself an erratic day", 'https://lefunny.net/wp-content/uploads/2014/08/funny-people-pics.jpg', "Horst", 35, 'AZ', 'Hair Styling', 'Hip Hop');
+
+new Profiles("Be extravagant", 'https://image.shutterstock.com/image-photo/funny-portrait-hero-260nw-410898763.jpg', "Heribert", 45, 'Austria', 'Photography', 'All');
+
+new Profiles("Dads will soon spread immortality", 'https://i.ytimg.com/vi/7__ZbrF58kI/hqdefault.jpg', "Olaf", 28, 'Japan', 'Photography', 'Metal');
+
+new Profiles("Always investigate a kick in the groin", 'https://4.bp.blogspot.com/-8va7RpZNSF8/UpIZdO988PI/AAAAAAAAFG4/s63zq2d9eQw/s1600/new+funny+girls.jpg', "Eins", 23, 'Austria', 'Travel', 'Pop');
+
+new Profiles("The answer to world hunger is nihilism", 'https://www.askideas.com/media/17/Carrie-Underwood-With-Upside-Down-Head-Funny-Photoshopped-Image.png', "Zwei", 32, 'NV', 'Electronics', 'Country');
+
+new Profiles("Respect a fraud", 'https://images.unsplash.com/photo-1583802625550-f9f11adcbe35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', "Drei", 21, 'South Africa', 'Travel', 'Rock');
+
+new Profiles("Fears are nice", 'https://images.unsplash.com/photo-1601705776515-b55faf59eb09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', "Vier", 92, 'Austria', 'Photography', 'All');
+
+
+console.table(profileArr);
+
+for (let i in profileArr) {
+    $('#result').append(profileArr[i].render(i));
+}
+
+
+
+for (let i in profileArr) {
+    $(`#favCard${i}`).on("click",function(){
+        $(`.removeP${i}`).remove(`.removeP${i}`);
+        $(`.myClass${i}`).html(profileArr[i].render2())
+    });
+}
+
+for (let i in profileArr) {
+    $(`#favCard${i}`).on("dblclick",function(){
+        $(`.favourite`).html(profileArr[i].render(i));
+        $(`.removeP${i}`).remove(`.removeP${i}`);
+        $(`.myClass${i}`).html(profileArr[i].render2(i));
+    });
+}
+
+
+
+
+
